d40c403c09e5d68859ca45f5a1527ca120c6e632
DESKTOP-NM4MKE2\DELL <m.abdelrasoul@ithmaar-solutions.com>
2025-05-07 01:45:31 +0300

    init project

---
+++ src/main/java/com/efada/EfadaApplication.java
@@ -0,0 +1,13 @@
+package com.efada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/efada/EfadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
