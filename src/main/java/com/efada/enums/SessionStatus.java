34b8237e3f8921a5855a520c6d8f232ef8859981
DESKTOP-NM4MKE2\DELL <m.abdelrasoul@ithmaar-solutions.com>
2025-05-07 12:42:52 +0300

    Add enums

---
+++ src/main/java/com/efada/enums/SessionStatus.java
@@ -0,0 +1,7 @@
+package com.efada.enums;
+
+public enum SessionStatus {
+	PENDING,
+    APPROVED,
+    REJECTED
+}
---
+++ src/main/java/com/efada/enums/UserRole.java
@@ -0,0 +1,7 @@
+package com.efada.enums;
+
+public enum UserRole {
+	ADMIN,
+    SPEAKER,
+    ATTENDEE
+}
